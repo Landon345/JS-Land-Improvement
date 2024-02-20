dd7f141032c7f781c5c857eb3125e18dd605e4e1
Landon Schlangen <lschlangen5@gmail.com>
2024-02-20 15:01:37 -0600

    New code

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -75,7 +75,6 @@
     src="https://d3ey4dbjkt2f6s.cloudfront.net/assets/static_link/work_request_embed_snippet.js"
     clienthub_id="1852624f-2f6b-48cb-ab45-9ad2f5c085e5"
     form_url="https://clienthub.getjobber.com/client_hubs/1852624f-2f6b-48cb-ab45-9ad2f5c085e5/public/work_request/embedded_work_request_form"
-    defer
   ></script>
 </div>
 
