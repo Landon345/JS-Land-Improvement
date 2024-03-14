1712fe5244a0bea3ac87f4312c25dd0af4df2245
Landon Schlangen <lschlangen5@gmail.com>
2024-03-13 20:30:15 -0500

    Move script inside head

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -37,6 +37,15 @@
   };
 </script>
 
+<svelte:head>
+  <script
+    async
+    src="https://d3ey4dbjkt2f6s.cloudfront.net/assets/static_link/work_request_embed_snippet.js"
+    clienthub_id="1852624f-2f6b-48cb-ab45-9ad2f5c085e5"
+    form_url="https://clienthub.getjobber.com/client_hubs/1852624f-2f6b-48cb-ab45-9ad2f5c085e5/public/work_request/embedded_work_request_form"
+  ></script>
+</svelte:head>
+
 <div class="contact-container">
   <div class="reach-here">
     <h3 class="contact-header">CONTACT US</h3>
@@ -71,11 +80,6 @@
     href="https://d3ey4dbjkt2f6s.cloudfront.net/assets/external/work_request_embed.css"
     media="screen"
   />
-  <script
-    src="https://d3ey4dbjkt2f6s.cloudfront.net/assets/static_link/work_request_embed_snippet.js"
-    clienthub_id="1852624f-2f6b-48cb-ab45-9ad2f5c085e5"
-    form_url="https://clienthub.getjobber.com/client_hubs/1852624f-2f6b-48cb-ab45-9ad2f5c085e5/public/work_request/embedded_work_request_form"
-  ></script>
 </div>
 
 <style>
